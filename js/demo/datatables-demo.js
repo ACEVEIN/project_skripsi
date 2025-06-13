0bcb8df062b42d69b8c5d7b23baad31663d1aaa4
Kevin Romelta <kevin.romelta@napinfo.co.id>
2025-06-13 17:40:29 +0700

    perbaikan head link dan script dan juga pada menu table

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,25 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+$(document).ready(function() {
+  $('#dataTable2').DataTable({
+    scrollX: true,
+  });
+});
+
+$(document).ready(function() {
+  $('#dataTable3').DataTable({
+    scrollX: true,
+    layout: {
+      topStart: {
+          buttons: ['copy', 'excel', 'pdf', 'colvis'],
+          pageLength: {
+                menu: [5, 10, 25, 50]
+            }
+      }
+    }
+    
+    
+  });
+});
